343ffc5497e2c4c77765c3c565d6990646500fee
redohariyadi24 <95567329+redohariyadi24@users.noreply.github.com>
2023-09-11 17:30:36 +0700

    config.php

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php
-class config{
+class config {
+    // Properti-properitas berikut digunakan untuk mengkonfigurasi koneksi ke database.
+    
+    // Nama host atau alamat IP dari server database.
     protected $host = 'localhost';
+
+    // Nama pengguna untuk menghubungkan ke database.
     protected $user = 'root';
+
+    // Kata sandi untuk mengautentikasi pengguna ke database.
     protected $password = '';
+
+    // Nama database yang akan dihubungkan.
     protected $database = 'db_tokomakmur';
 }
 ?>
