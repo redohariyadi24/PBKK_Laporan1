88b78c8fd40d7c058e76667d4140a7c0c283c2bf
redohariyadi24 <95567329+redohariyadi24@users.noreply.github.com>
2023-09-11 17:36:36 +0700

    pelanggan.php

--- pelanggan.php
+++ pelanggan.php
@@ -1,19 +1,24 @@
 <?php
-    require_once "config.php";
-    class pelanggan extends config{
-        private $koneksi;
+require_once "config.php";
 
-    public function __construct(){
+class pelanggan extends config {
+    private $koneksi;
+
+    public function __construct() {
+        // Membuat koneksi ke database menggunakan informasi yang didefinisikan dalam kelas config
         $this->koneksi = mysqli_connect($this->host, $this->user, $this->password, $this->database);
     }
 
-    function dataPelanggan(){
+    // Fungsi untuk mengambil data pelanggan dari database
+    function dataPelanggan() {
         $query = mysqli_query($this->koneksi, 'SELECT * FROM pelanggan');
-        while($row = mysqli_fetch_array($query)){
-            $data[]=$row;
+        $data = array(); // Inisialisasi array data untuk menampung hasil
+
+        while ($row = mysqli_fetch_array($query)) {
+            $data[] = $row; // Menambahkan setiap baris data ke dalam array data
         }
 
-        return $data;
+        return $data; // Mengembalikan array data pelanggan
     }
 }
 ?>
